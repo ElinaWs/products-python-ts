6bba3e5971889af284c17cefc480a0a6c576f312
Student <student@compass.local>
2026-04-14 11:18:07 +0600

    add aythorixation

--- src/axiosApi.ts
+++ src/axiosApi.ts
@@ -5,3 +5,14 @@ export const axiosApi = axios.create(
         baseURL: 'http://127.0.0.1:8000/api/v1'
     }
 )
+
+axiosApi.interceptors.request.use(request => {
+    const token = localStorage.getItem('token');
+
+    if(token) {
+        request.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return request;
+}, error => {
+    return Promise.reject(error)
+})
--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router"
 
 export const ProductsPage = () => {
     const [products, setProducts] = useState<IProduct[]>([])
+    const [myEmail, setMyEmail] = useState('');
 
     useEffect( () => {
       const getProducts = async() => {
@@ -19,21 +20,35 @@ export const ProductsPage = () => {
       getProducts()
     }, [])
 
+    useEffect(() => {
+        const getMyEmail = async() => {
+            try {
+                const response  = await axiosApi('/users/protected');
+                const data = response.data;
+                setMyEmail(data.messege)
+            } catch(e) {
+            console.log(e)
+        }
+    }
+    getMyEmail();
+    }, [])
+
     return (
-<div>
-    <Link to='/product/create'>
-        Create Product
-    </Link>
-    <div style={{display: 'flex', flexWrap: 'wrap', gap: '8px'}}>
-            {
-                products.map((productItem) => (
-                    <ProductCard
-                        product={productItem}
-                        key={productItem.id}
-                    />
-                ))
-            }
+        <div>
+            <Link to='/product/create'>
+                Create Product
+            </Link>
+            {myEmail && <h1>My Email: {myEmail}</h1>}
+            <div style={{display: 'flex', flexWrap: 'wrap', gap: '8px'}}>
+                    {
+                        products.map((productItem) => (
+                            <ProductCard
+                                product={productItem}
+                                key={productItem.id}
+                            />
+                        ))
+                    }
+                </div>
         </div>
-</div>
     )
 }
